e0b3f97285f3fdd90737d38acae1f80ff76d28ea
slavb18 <slavb18@gmail.com>
2014-11-23 12:49:21 +0400

    xtag.register with extends

--- src/form-ajax.js
+++ src/form-ajax.js
@@ -10,43 +10,47 @@
         }
         return null;
     };
-    // create hidden input with clicked submit button name and value attributes
-    xtag.addEvent(document, 'click:delegate(form[is=form-ajax] button[type=submit])', function (event) {
-        var form=this.form;
-        if (!form.hiddenSubmitEl) {
-            form.hiddenSubmitEl=document.createElement('input');
-            form.hiddenSubmitEl.setAttribute('type', 'hidden');
-            form.appendChild(form.hiddenSubmitEl);
-        }
-        form.hiddenSubmitEl.setAttribute('name', this.name);
-        form.hiddenSubmitEl.setAttribute('value', this.value);
-    });
-    xtag.addEvent(document, 'submit:delegate(form[is=form-ajax])', function (event) {
-        event.preventDefault();
-        event.stopPropagation();
-        var method = getValidMethod(this.getAttribute('method'));
-        if (!method) {
-            throw new Error('Invalid method!');
-        }
-        var action = this.getAttribute('action');
-        if (!action) {
-            throw new Error('Invalid action!');
-        }
-        var form = this;
-        if (this.getAttribute("enctype") === "application/json") {
-            applicationJSON(this, function (json) {
-                form.hiddenSubmitEl.removeAttribute("name");
-                var request = new XMLHttpRequest();
-                request.onreadystatechange = function () {
-                    if (request.readyState === 4) {
-                        xtag.fireEvent(form, 'submitted', {detail: {request: request, data: json}});
-                    }
-                };
-                request.open(method, action, true);
-                request.setRequestHeader('Content-Type', "application/json");
-                request.send(JSON.stringify(json, null, 4));
-            });
-        }
-    });
+    xtag.register('form-ajax', {
+        extends: 'form',
+        events: {
+            'click:delegate(button[type=submit])': function (event) {
+                var form = this.form;
+                if (!form.hiddenSubmitEl) {
+                    form.hiddenSubmitEl = document.createElement('input');
+                    form.hiddenSubmitEl.setAttribute('type', 'hidden');
+                    form.appendChild(form.hiddenSubmitEl);
+                }
+                form.hiddenSubmitEl.setAttribute('name', this.name);
+                form.hiddenSubmitEl.setAttribute('value', this.value);
+            },
+            'submit': function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                var method = getValidMethod(this.getAttribute('method'));
+                if (!method) {
+                    throw new Error('Invalid method!');
+                }
+                var action = this.getAttribute('action');
+                if (!action) {
+                    throw new Error('Invalid action!');
+                }
+                var form = this;
+                if (this.getAttribute("enctype") === "application/json") {
+                    applicationJSON(this, function (json) {
+                        form.hiddenSubmitEl.removeAttribute("name");
+                        var request = new XMLHttpRequest();
+                        request.onreadystatechange = function () {
+                            if (request.readyState === 4) {
+                                xtag.fireEvent(form, 'submitted', {detail: {request: request, data: json}});
+                            }
+                        };
+                        request.open(method, action, true);
+                        request.setRequestHeader('Content-Type', "application/json");
+                        request.send(JSON.stringify(json, null, 4));
+                    });
+                }
 
+            }
+        }
+    });
 })();
